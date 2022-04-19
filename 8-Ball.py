92dc4c870adf5b44921c6d07bc1eeadd31f95541
N1TR5 <83928701+PogManation@users.noreply.github.com>
2022-04-19 13:29:00 -0500

    Create 8-Ball.py

---
+++ 8-Ball.py
@@ -0,0 +1,6 @@
+import random
+
+responses = ["Yes", "No", "Probably", "Probably not", "I don't know", "Ask someone else"]
+you = input("Ask the 8ball a question: ")
+if you == you:
+  print(random.choice(responses))
